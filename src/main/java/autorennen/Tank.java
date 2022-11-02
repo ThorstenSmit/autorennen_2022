47e6e89948388598e460d2f8a5d426db5cb76582
Jennifer <Jennifer@10.231.19.107>
2022-11-02 11:58:49 +0100

    Klasse Auto & Tank Methoden erstellt

--- src/main/java/autorennen/Tank.java
+++ src/main/java/autorennen/Tank.java
@@ -1,4 +1,12 @@
 package autorennen;
 public class Tank {
-    
+    private float tankstand;
+    private int tankstandMaximal = 100;
+    private int genormterVerbrauch  = 5;
+
+    public float getTankstand() {
+        return tankstand;
+    }
+    public void tankVerbrauch(int geschwindigkeit){  
+    }
 }
